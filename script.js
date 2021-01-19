fc4f9d0c6216cbadbf0cbe7ce9f00d1579faf16e
Devansh Patil <subtra3t.dev@gmail.com>
2021-01-19 10:11:03 +0530

    Fixed animation using js

---
+++ script.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", () => {
+  const first = document.getElementById("first");
+  const second = document.getElementById("second");
+  first.id = "first-updated";
+  second.id= "second-updated";
+});
