8a411381e10c17c5e84c47d426d3449a5b17022e
Devansh Patil <subtra3t.dev@gmail.com>
2021-01-21 15:58:16 +0530

    Fixed bug

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 window.addEventListener("load", () => {
   const first = document.getElementById("first");
   const second = document.getElementById("second");
-  first.id = "first-updated";
-  second.id= "second-updated";
+  first.style.animationName = "fade-in";
+  first.style.animationDuration = "1.25s";
+  first.style.animationDelay = "0.25s";
+  first.style.animationIterationCount: "1";
+  second.style.animationName = "fade-in";
+  second.style.animationDuration = "1s";
+  second.style.animationDelay = "1.5s";
+  second.style.animationIterationCount: "1";
 });
