fdb2087c928762d785b70cd7ccf755f81b56e1ef
Devansh Patil <subtra3t.dev@gmail.com>
2021-01-25 16:54:06 +0530

    Initialized terminal

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 window.addEventListener("load", () => {
-  const first = document.getElementById("first");
-  const second = document.getElementById("second");
-  first.style.animationName = "fade-in";
-  first.style.animationDuration = "1.25s";
-  first.style.animationDelay = "0.25s";
-  first.style.animationIterationCount = "1";
-  first.id = "first-updated";
-  second.style.animationName = "fade-in";
-  second.style.animationDuration = "1s";
-  second.style.animationDelay = "1.5s";
-  second.style.animationIterationCount = "1";
-  second.id = "second-updated";
+  const terminal = new TypeWriter(document.getElementById("type-term"), {loop: false});
+  setTimeout(() => {
+    terminal.typeString("Hello World")
+      .pausefor(500)
+      .deleteChars(6)
+      .typeString(". My name is <strong>Devansh Patil</strong>")
+      .pauseFor(1250)
+      .deleteChars(24)
+      .typeString("Take a look at my portfolio site and some of my projects.")
+      .start();
+  }, 750);
 });
