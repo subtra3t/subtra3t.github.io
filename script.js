2939001ecd4d833d276c51f4926aa98a2ffde826
Devansh Patil <subtra3t.dev@gmail.com>
2021-03-08 16:44:04 +0530

    Removed name

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ window.addEventListener("load", () => {
     terminal.typeString("Hello World")
       .pauseFor(500)
       .deleteChars(6)
-      .typeString(". My name is <strong>Devansh Patil</strong>")
+      .typeString(". My name is <strong>subtra3t</strong>")
       .pauseFor(1250)
       .deleteChars(24)
       .typeString("Take a look at my portfolio site and some of my projects.")
